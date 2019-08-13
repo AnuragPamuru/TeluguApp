a0dc86db3adc30ecd2c39a2e129eacb9ad230d29
Anurag Pamuru <anuragpamuru0129@gmail.com>
2019-08-12 19:10:26 -0700

    Adding dark mode features, unsuccessful?

--- app/src/main/java/com/example/teluguapp/MainActivity.kt
+++ app/src/main/java/com/example/teluguapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.teluguapp
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settings = findViewById<ImageButton>(R.id.imageButton)
+        settings.setOnClickListener{openSettings()}
+        settings.setBackgroundColor(Color.WHITE)
+    }
+
+    fun openSettings(){
+        val intent = Intent(this,SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/teluguapp/SettingsActivity.kt
+++ app/src/main/java/com/example/teluguapp/SettingsActivity.kt
@@ -1,24 +1,21 @@
 package com.example.teluguapp
 
-import android.os.Bundle
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.settings, SettingsFragment())
-            .commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setContentView(R.layout.activity_settings)
+        val toggle = findViewById<ImageButton>(R.id.toggleButton)
+        toggle.setOnClickListener{toggle.setImageResource(R.drawable.ic_invert_w)}
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
+    fun toggleMode(){
+
     }
 }
