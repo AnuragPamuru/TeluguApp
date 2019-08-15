f0652dfac2e63c675f8329b490d36ae67573ee84
Anurag Pamuru <anuragpamuru0129@gmail.com>
2019-08-15 12:49:35 -0700

    Edits to the settings activity

--- app/src/main/java/com/example/teluguapp/SettingsActivity.kt
+++ app/src/main/java/com/example/teluguapp/SettingsActivity.kt
@@ -1,10 +1,12 @@
 package com.example.teluguapp
 
+import android.graphics.Color
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -12,10 +14,25 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val toggle = findViewById<ImageButton>(R.id.toggleButton)
-        toggle.setOnClickListener{toggle.setImageResource(R.drawable.ic_invert_w)}
+        toggle.setImageResource(R.drawable.ic_invert_b)
+        toggle.setBackgroundColor(Color.WHITE)
+        toggle.setOnClickListener{toggleModeW()}
     }
 
-    fun toggleMode(){
+    fun toggleModeW(){
+        val toggle = findViewById<ImageButton>(R.id.toggleButton)
+        toggle.setImageResource(R.drawable.ic_invert_w)
+        toggle.setBackgroundColor(Color.BLACK)
+        toggle.setOnClickListener{toggleModeB()}
+
 
     }
+
+    fun toggleModeB(){
+        val toggle = findViewById<ImageButton>(R.id.toggleButton)
+        toggle.setImageResource(R.drawable.ic_invert_b)
+        toggle.setBackgroundColor(Color.WHITE)
+        toggle.setOnClickListener{toggleModeW()}
+    }
+
 }
