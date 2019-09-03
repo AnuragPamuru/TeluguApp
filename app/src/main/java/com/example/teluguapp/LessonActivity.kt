f9d384701bdfe31f167c6ef4f6a4129adf817f2d
Anurag Pamuru <anuragpamuru0129@gmail.com>
2019-09-02 23:07:28 -0700

    testing bugs with buttons

--- app/src/main/java/com/example/teluguapp/LessonActivity.kt
+++ app/src/main/java/com/example/teluguapp/LessonActivity.kt
@@ -23,26 +23,34 @@ class LessonActivity : AppCompatActivity() {
         val setUpData = resources.openRawResource(setUpFile)
         val reader = BufferedReader(InputStreamReader(setUpData, "Unicode"))
 
+//        for(i in reader.lineSequence()){
+//            //use each line to set up words
+//
+//            //read the letters, meaning, and sound path
+//            val data = i.split("\\t")
+//
+//            val translations = data[1].split(",")
+//            val word = Word(data[0], translations, data[2].toInt(),this)
+//
+//            val myButton = Button(this)
+//            myButton.text = word.word
+//            myButton.setOnClickListener { word.playSound() }
+//            val ll = findViewById<ConstraintLayout>(R.id.activity_lesson)
+//            val lp = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+//            ll.addView(myButton, lp)
+//        }
 
-        val list = mutableListOf<Word>()
+        val data = reader.readLine().split("\\t")
 
-        for(i in reader.lineSequence()){
-            //use each line to set up words
+        val translations = data[1].split(",")
+        val word = Word(data[0], translations, data[2].toInt(),this)
 
-            //read the letters, meaning, and sound path
-            val data = i.split("\\t")
-
-            val translations = data[1].split(",")
-            val word = Word(data[0], translations, data[2].toInt(),this)
-
-            val myButton = Button(this)
-            myButton.text = word.word
-            myButton.setOnClickListener { word.playSound() }
-            val ll = findViewById<ConstraintLayout>(R.id.activity_lesson)
-            val lp = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
-            ll.addView(myButton, lp)
-        }
-
+        val myButton = Button(this)
+        myButton.text = word.word
+        myButton.setOnClickListener { word.playSound() }
+        val ll = findViewById<ConstraintLayout>(R.id.activity_lesson)
+        val lp = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        ll.addView(myButton, lp)
         //testing - feed those words into buttons
 
 
