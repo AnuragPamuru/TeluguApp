31f00c1ba1b98ba4c33226489e55c345dbc3113c
Anurag Pamuru <anuragpamuru0129@gmail.com>
2019-09-13 15:14:08 -0700

    Added search feature buttons

--- app/src/main/java/com/example/teluguapp/SearchFragment.kt
+++ app/src/main/java/com/example/teluguapp/SearchFragment.kt
@@ -1,14 +1,18 @@
 package com.example.teluguapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
 
 
 class SearchFragment : Fragment() {
 
+    lateinit var searchString : String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,10 +23,14 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setUpSearchEngine()
+
+        val searchButton = activity?.findViewById(R.id.search_button) as ImageButton
+        searchButton.setOnClickListener{runSearchEngine()}
     }
 
-    private fun setUpSearchEngine() {
-
+    private fun runSearchEngine() {
+        val editText = activity?.findViewById(R.id.searchBar) as EditText
+        searchString = editText.text.toString()
+        println(searchString)
     }
 }
